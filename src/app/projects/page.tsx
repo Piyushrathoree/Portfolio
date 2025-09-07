b28e586a44e22043fa914dd6c582d4bfafb6863e
PIYUSH RATHORE <163632958+Piyushrathoree@users.noreply.github.com>
2025-09-07 10:23:16 +0530

    Update page.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,7 +8,7 @@ const page = () => {
             title: "Welth",
             description: "An AI - powered finance management platform",
             video: "/welth.mp4",
-            link: "https://welth-ai-finance-app-eight.vercel.app/",
+            link: "https://welth-ai-finance-app-liard.vercel.app/",
             github: "https://github.com/Piyushrathoree/Welth-ai-finance-app",
             tags: [
                 "React",
